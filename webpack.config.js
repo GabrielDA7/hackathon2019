bbcdf562daa9830c0da89111e795d337ab15db03
Gabriel <gabriel.pro.d3@gmail.com>
2019-03-19 14:09:09 +0100

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ Encore
      * (including one that's included on every page - e.g. "app")
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('landing_index', './assets/js/landing/index.js')
@@ -35,7 +35,7 @@ Encore
 //.enableTypeScriptLoader()
 
 // uncomment if you use Sass/SCSS files
-//.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
